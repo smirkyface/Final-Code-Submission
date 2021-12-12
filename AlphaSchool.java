8f9ec9d8602104a1138193302d6ca890184cdc52
smirkyface <72003137+smirkyface@users.noreply.github.com>
2021-12-11 21:52:55 -0600

    Add files via upload

---
+++ AlphaSchool.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package alphaschool;
+
+/**
+ * @author Erik Thompson
+ * @author Kachi Mbamalu
+ * @author Jonathan Ruiz
+ * @author Matthew Davis
+ */
+
+public class AlphaSchool 
+{
+    public static void main(String[] args) 
+    {
+        new HomeScreen();
+    }
+    
+}
